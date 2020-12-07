312508568135ff74f8f012a0e398d7131f6eaebe
Mark Bradshaw <notthehoople@googlemail.com>
2020-12-07 20:02:30 +0000

    Day 07 part a complete

--- cmd/day07/day07.go
+++ cmd/day07/day07.go
@@ -11,21 +11,19 @@ func readSingleRule(ruleLine string, bagRules map[string]map[string]int, part by
 	// split the remainder on ","
 	//   - process each of the remaining strings as a bag
 
-	//var bagRules map[string]map[string]int
 	var topLevelBag string
-	//bagRules = make(map[string]map[string]int)
 
 	initialSplit := strings.Split(ruleLine, "contain")
-	//fmt.Println("First string:", initialSplit[0])
-	//fmt.Println("Remainder:", strings.Trim(initialSplit[1], " ."))
 
 	topLevelBag = strings.Trim(initialSplit[0], " ")
+	topLevelBag = strings.TrimSuffix(topLevelBag, " bags")
+	topLevelBag = strings.TrimSuffix(topLevelBag, " bag")
 	bagRules[topLevelBag] = make(map[string]int)
 
-	// Note need to either deal with "bags" vs "bag" here or during search
-
 	if strings.Trim(initialSplit[1], " .") == "no other bags" {
-		fmt.Println("No other bags so stop processing")
+		if debug {
+			fmt.Println("No other bags so stop processing")
+		}
 	} else {
 		for _, item := range strings.Split(initialSplit[1], ",") {
 			var numBags int = 0
@@ -33,31 +31,70 @@ func readSingleRule(ruleLine string, bagRules map[string]map[string]int, part by
 
 			item = strings.Trim(item, " .")
 
+			// Grab the number, plus trim spaces, full stops and get rid of bag/bags
 			fmt.Sscanf(item, "%d %s", &numBags, &lowerLevelBag)
 			lowerLevelBag = item[strings.IndexByte(item, ' ')+1:]
+			lowerLevelBag = strings.TrimSuffix(lowerLevelBag, " bags")
+			lowerLevelBag = strings.TrimSuffix(lowerLevelBag, " bag")
 
 			bagRules[topLevelBag][lowerLevelBag] = numBags
 		}
 	}
 }
 
+func searchBagsRecurse(bagRules map[string]map[string]int, topLevelBag string, bagToSearch string) int {
+	var foundCount int = 0
+
+	for thing := range bagRules[topLevelBag] {
+
+		if thing == bagToSearch {
+			//fmt.Println("Got it in searchBagsRecurse")
+			return 1
+		}
+
+		foundCount = searchBagsRecurse(bagRules, thing, bagToSearch)
+		if foundCount > 0 {
+			return 1
+		}
+	}
+	return 0
+}
+
+func searchBags(bagRules map[string]map[string]int, bagToSearch string) int {
+	var foundBagsCount int = 0
+
+	for thing := range bagRules {
+		if thing == bagToSearch {
+			// If we find it at the top level we ignore it as needs to be INSIDE another bag
+			//fmt.Println("Got it in searchBags")
+		} else {
+			foundBagsCount += searchBagsRecurse(bagRules, thing, bagToSearch)
+		}
+	}
+
+	return foundBagsCount
+}
+
 // Process the whole file of bag rules
 // Build a map of maps containing the rules
-func processAllBagRules(filename string, part byte, debug bool) int {
+func processAllBagRules(filename string, bagToSearch string, part byte, debug bool) int {
 	var bagRules map[string]map[string]int
 	bagRules = make(map[string]map[string]int)
 
 	puzzleInput, _ := readFile(filename)
 
 	for _, singleRule := range puzzleInput {
-		//fmt.Println("Single Rule:", singleRule)
-		//fmt.Println("bagRules:", bagRules)
 		readSingleRule(singleRule, bagRules, part, debug)
 	}
 
-	fmt.Println("Bag Rules:", bagRules)
+	bagToSearch = strings.TrimSuffix(bagToSearch, " bags")
+	bagToSearch = strings.TrimSuffix(bagToSearch, " bag")
+	if debug {
+		fmt.Println("Bag to Search for:", bagToSearch)
+		fmt.Println("Bag Rules:", bagRules)
+	}
 
-	return 0
+	return searchBags(bagRules, bagToSearch)
 }
 
 // Main routine
@@ -66,7 +103,7 @@ func main() {
 	if execPart == 'z' {
 		fmt.Println("Bad part choice. Available choices are 'a' and 'b'")
 	} else if execPart == 'a' {
-		fmt.Println("Bag Varieties:", processAllBagRules(filenamePtr, execPart, debug))
+		fmt.Println("Bag Varieties:", processAllBagRules(filenamePtr, "shiny gold bag", execPart, debug))
 	} else {
 		fmt.Println("Not implemented yet")
 		fmt.Println(filenamePtr, execPart, debug)
