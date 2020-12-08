a104d2cdf732bd7bc8a1ba08977de721d6e8a5d2
Mark Bradshaw <markbradshaw@markbrad02m.home>
2020-12-08 09:31:43 +0000

    Removed unneccessary routines from day 06

--- cmd/day06/day06.go
+++ cmd/day06/day06.go
@@ -25,61 +25,6 @@ func catchUserInput() (string, byte, bool) {
 	}
 }
 
-func calcRange(calcChar rune, lowerLimit int, upperLimit int, debug bool) (int, int) {
-	var newSize int
-	var newLowerLimit, newUpperLimit int
-
-	size := upperLimit - lowerLimit + 1
-	if debug {
-		fmt.Printf("Before calc... size: %d lowerLimit: %d upperLimit: %d\n", size, lowerLimit, upperLimit)
-	}
-
-	newSize = size / 2
-	if calcChar == 'F' || calcChar == 'L' {
-		newLowerLimit = lowerLimit
-		newUpperLimit = lowerLimit + newSize - 1
-	} else {
-		newLowerLimit = lowerLimit + newSize
-		newUpperLimit = upperLimit
-	}
-
-	if debug {
-		fmt.Printf("After calc... size: %d lowerLimit: %d upperLimit: %d\n", newSize, newLowerLimit, newUpperLimit)
-	}
-	return newLowerLimit, newUpperLimit
-}
-
-func decodeSinglePass(boardingPass string, part byte, debug bool) int {
-	if debug {
-		fmt.Println("Processing: ", boardingPass)
-	}
-
-	//var rows int = 128
-	var lowerLimit int = 0
-	var upperLimit int = 127
-	var rowNumber int = 0
-	for _, rowChar := range boardingPass[0:7] {
-		if debug {
-			fmt.Printf("Row Instruction: %c\n", rowChar)
-		}
-		lowerLimit, upperLimit = calcRange(rowChar, lowerLimit, upperLimit, debug)
-	}
-	rowNumber = lowerLimit
-
-	lowerLimit = 0
-	upperLimit = 7
-	var columnNumber int = 0
-	for _, columnChar := range boardingPass[7:] {
-		if debug {
-			fmt.Printf("Column Instruction: %c\n", columnChar)
-		}
-		lowerLimit, upperLimit = calcRange(columnChar, lowerLimit, upperLimit, debug)
-	}
-	columnNumber = lowerLimit
-
-	return (rowNumber * 8) + columnNumber
-}
-
 func processSinglePersonAnswers(answers string, answersStore map[byte]int, part byte, debug bool) {
 	// Build a map of the questions
 
