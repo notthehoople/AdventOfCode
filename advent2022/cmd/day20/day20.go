191e4a78db3742e798cf579dd80954bf652b947b
Mark Bradshaw <notthehoople@googlemail.com>
2022-12-24 21:09:36 +0000

    Day20 part b ongoing

--- advent2022/cmd/day20/day20.go
+++ advent2022/cmd/day20/day20.go
@@ -32,11 +32,15 @@ func buildGroveRing(numbersList []int, debug bool) map[position]*ring.Ring {
 }
 
 func calcGroveCoords(filename string, part byte, debug bool) int {
+	const decryptKey int = 811589153
 
 	puzzleInput, _ := utils.ReadFile(filename)
 	numbersList := make([]int, len(puzzleInput))
 	for i := 0; i < len(puzzleInput); i++ {
 		numbersList[i], _ = strconv.Atoi(puzzleInput[i])
+		if part == 'b' {
+			numbersList[i] *= decryptKey
+		}
 	}
 
 	// We need a map to point into the ring so we don't lose track of where things are as we're moving things around
@@ -44,21 +48,28 @@ func calcGroveCoords(filename string, part byte, debug bool) int {
 	zeroPos := position{}
 
 	// Loop through the list of numbers, using them to look into the map to get the item to be moved
-
-	for index, number := range numbersList {
-		// We need the zero pos so we can calc the coords starting from 0. Grab it when we see it go passed
-		if number == 0 {
-			zeroPos.index = index
-			zeroPos.number = 0
-		}
-
-		// Grab the loop pointer for the position we're at in the list of numbers
-		ringPos := groveCoords[position{index: index, number: number}].Prev()
-
-		movingItem := ringPos.Unlink(1)
-		ringPos.Move(number).Link(movingItem)
+	var loopCount int = 1
+	if part == 'b' {
+		loopCount = 10
 	}
 
+	for loop := 0; loop < loopCount; loop++ {
+		fmt.Println("Loop:", loop)
+
+		for index, number := range numbersList {
+			// We need the zero pos so we can calc the coords starting from 0. Grab it when we see it go passed
+			if number == 0 {
+				zeroPos.index = index
+				zeroPos.number = 0
+			}
+
+			// Grab the loop pointer for the position we're at in the list of numbers
+			ringPos := groveCoords[position{index: index, number: number}].Prev()
+
+			movingItem := ringPos.Unlink(1)
+			ringPos.Move(number).Link(movingItem)
+		}
+	}
 	// Now use the zeroPos we grabbed earlier as the starting point. Do 3 x move(1000) and grab the value from each
 
 	var groveCoordsResult int
@@ -81,7 +92,7 @@ func main() {
 	case 'a':
 		fmt.Printf("Grove Coords: %d\n", calcGroveCoords(filenamePtr, execPart, debug))
 	case 'b':
-		fmt.Printf("Not implemented yet\n")
+		fmt.Printf("Grove Coords: %d\n", calcGroveCoords(filenamePtr, execPart, debug))
 	case 'z':
 		fmt.Println("Bad part choice. Available choices are 'a' and 'b'")
 	}
