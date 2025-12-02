af8c0ec0f0b6514943147d1bce50a09f89b17dfb
Mark Bradshaw <notthehoople@googlemail.com>
2025-12-02 19:39:56 +0000

    Day02 part b done

--- advent2025/cmd/day02/day02.go
+++ advent2025/cmd/day02/day02.go
@@ -20,11 +20,11 @@ func day02(filename string, part byte, debug bool) int {
 		}
 
 		extremes := strings.Split(puzzleItem, "-")
+		firstID, _ := strconv.Atoi(extremes[0])
+		lastID, _ := strconv.Atoi(extremes[1])
 
 		if part == 'a' {
-			firstID, _ := strconv.Atoi(extremes[0])
-			lastID, _ := strconv.Atoi(extremes[1])
-
+			// Look for the first half of a testID being the same as the second
 			for testID := firstID; testID <= lastID; testID++ {
 				if debug {
 					fmt.Println("testID:", testID)
@@ -37,7 +37,7 @@ func day02(filename string, part byte, debug bool) int {
 
 					if strings.Count(testIDstr, compare) > 1 {
 						if debug {
-							fmt.Println("Valid ID:", testIDstr)
+							fmt.Println("Invalid ID:", testIDstr)
 						}
 
 						result += testID
@@ -45,14 +45,37 @@ func day02(filename string, part byte, debug bool) int {
 				}
 			}
 		} else { // part b
+			// Repeats can be any length, including 1 character. We can't filter out strings that have an odd number of characters
+			// Try brute force approach to start
+			for testID := firstID; testID <= lastID; testID++ {
+				// Starting with 1, increment through patterns of more an more characters looking for a match
+				// We can filter out whenever the pattern length doesn't divide into the overall length of the testID
 
+				testIDstr := strconv.Itoa(testID)
+
+				for patternSize := 1; patternSize <= len(testIDstr)/2; patternSize++ {
+					compare := testIDstr[0:patternSize]
+					if debug {
+						fmt.Println("testID:", testID, "patternSize:", patternSize, "compare:", compare)
+					}
+
+					// If the length of the patternSize exactly divides into the testIDstr
+					// AND the compare string is present in a pattern in the testIDstr
+					// THEN we have an invalid ID
+					// - edge case - need to stop comparing when we've already matched an invalid ID so we don't count it again
+					if (len(testIDstr)%patternSize == 0) && (strings.Count(testIDstr, compare) == len(testIDstr)/patternSize) {
+						if debug {
+							fmt.Println("Invalid ID:", testIDstr)
+						}
+
+						result += testID
+
+						// Break out of the inner loop as we've found the invalid ID and don't need to try other options
+						break
+					}
+				}
+			}
 		}
-
-	}
-
-	if part == 'a' {
-
-		return result
 	}
 
 	return result
